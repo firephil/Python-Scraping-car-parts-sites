0a472b838a7e1bcde5cc6e32567c1e8766b84ce6
philip <philip@firesoft.gr>
2015-11-17 13:50:40 +0200

    changes

--- Partsone-Scraping/scrape/File.py
+++ Partsone-Scraping/scrape/File.py
@@ -19,6 +19,7 @@ def rename_files(path, badprefix):
 def list_files(directory):
     return [os.path.join(directory, f) for f in os.listdir(directory)]
 
+
 def ls_tree(root):
     for root, dirs, files in os.walk(root, topdown=False):
         for name in files:
