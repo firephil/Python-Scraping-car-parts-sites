335d421efb06832f2d7ff96a5bffd8accfb51020
philip <philip@firesoft.gr>
2015-11-19 15:07:20 +0200

    testing

--- Partsone-Scraping/scrape/main.py
+++ Partsone-Scraping/scrape/main.py
@@ -4,4 +4,6 @@ import Aeival
 
 if __name__ == '__main__':
     # Aeival.scrape()
-    Aeival.scrape2()
+    # Aeival.scrape2()
+
+    Img.resize_all("C:\\Users\\PARTSONE\\Desktop\\Audi-post-NOE", 600)
